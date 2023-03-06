08d17b61a45e47996b30c4346b51f03346d2eaac
Ibtisam Mirza <iuhmirza@gmail.com>
2023-03-06 15:26:23 +0000

    Add postgres database and messages model

--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    message_test = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Reactions(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
