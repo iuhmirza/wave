0811f3734b27cffec10e9563180f48c059dd66ec
Ibtisam Mirza <iuhmirza@gmail.com>
2023-03-06 17:02:34 +0000

    Make admin page for messages

--- messages/models.py
+++ messages/models.py
@@ -1,11 +1,24 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 class Message(models.Model):
-    message_test = models.CharField(max_length=200)
+    def __str__(self):
+        return self.message_text
+
+    def is_recent(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    message_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-class Reactions(models.Model):
+class Reaction(models.Model):
+    def __str__(self):
+        return self.choice_text
+
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
